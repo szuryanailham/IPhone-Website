5b19ca7d883265c02f9492e29923bb5a703f06ae
ilham Suryana <szuryanailham090102@gmail.com>
2024-09-09 13:01:43 +0700

    fiture highlight in iphone website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Hightlight from "./components/Hightlight";
+const App = () => {
+  return (
+    <div className="bg-black">
+      <Navbar />
+      <Hero />
+      <Hightlight />
+    </div>
+  );
+};
+
+export default App;
