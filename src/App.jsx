8a280ee990267aed7510bb8bb8c234f6e6c99447
ilham Suryana <szuryanailham090102@gmail.com>
2024-09-12 10:48:45 +0700

    menambahkan component design website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Hightlight from "./components/Hightlight";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWork from "./components/HowItWork";
 const App = () => {
   return (
     <div className="bg-black">
       <Navbar />
       <Hero />
       <Hightlight />
+      <Model />
+      <Features />
+      <HowItWork />
     </div>
   );
 };
